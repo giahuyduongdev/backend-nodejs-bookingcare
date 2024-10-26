fb076b97339f77df1356df5fe4f35e323f088033
giahuyduongdev <giahuyduongdev@gmail.com>
2024-10-27 02:15:49 +0700

    CRUD User: Add a new user with ejs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
   try{
@@ -15,8 +16,20 @@ let getAboutPage = async (req, res) => {
   return res.render('test/about.ejs');
 }
 
+let getCRUD =  (req,res) =>{
+  return res.render("crud.ejs");
+}
+
+let postCRUD = async(req, res) =>{
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(req.body);
+  console.log(message);
+  return res.send('Post crud from server');
+}
 
 module.exports = {
   getHomePage: getHomePage,
-  getAboutPage: getAboutPage
+  getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
 }
