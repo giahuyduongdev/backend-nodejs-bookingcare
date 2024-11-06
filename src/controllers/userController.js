4ada0170689fb5f3fdf6db9b75362621cf4f97a2
giahuyduongdev <giahuyduongdev@gmail.com>
2024-11-07 00:36:14 +0700

    CRUD API for User

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -48,10 +48,35 @@ let handleGetAllUsers = async (req, res) =>{
       users: [],
     });
   }
-  
 };
+
+let handleCreateNewUser = async (req, res) => {
+  let message = await userService.createNewUser(req.body);
+  return res.status(200).json(message);
+};
+
+let handleDeleteUser = async (req, res) =>{
+  let id = req.body.id;
+  if(!id){
+    return res.status(500).json({
+      errCode: 1,
+      errMessage: 'Missing required parameters!'
+    })
+  }
+  let message = await userService.deleteUser(id);
+  return res.status(200).json(message);
+}
+
+let handleEditUser = async (req, res) =>{
+  let data = req.body;
+  let message = await userService.updateUserData(data);
+  return res.status(200).json(message);
+}
 
 module.exports = {
   handleLogin : handleLogin,
   handleGetAllUsers : handleGetAllUsers,
+  handleCreateNewUser : handleCreateNewUser,
+  handleEditUser : handleEditUser,
+  handleDeleteUser: handleDeleteUser
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -87,8 +87,120 @@ let checkUserEmail = (userEmail) => {
   });
 };
 
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPassword = await bcrypt.hashSync(password, salt);
+      //  Hàm bcrypt.hashSync có thể là một hoạt động bất đồng bộ, nghĩa là nó có thể mất một thời gian để hoàn thành. Sử dụng Promise và async/await cho phép code tiếp tục thực hiện các tác vụ khác trong khi chờ đợi quá trình hash hoàn thành.
+      resolve(hashPassword);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let createNewUser  = (data) =>{
+  return new Promise(async (resolve, reject) => {
+    try{
+      //check email first
+      let check = await checkUserEmail(data.email);
+      if (check == true)
+      {
+        resolve({
+          errCode: 1,
+          message: 'Your email is already in used, Plz try another email'
+        });
+      }else{
+        let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          gender: data.gender == "1" ? true : false,
+          roleId: data.roleId,
+          phonenumber: data.phonenumber,
+        });
+      }
+      resolve({
+        errCode: 0,
+        message: 'OK'
+      });
+    }catch(e){
+      reject(e);
+    }
+  });
+};
+
+let deleteUser = (userId) =>{
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: userId },
+      });
+      if (!user) {
+        resolve({
+          errCode: 2,
+          errMessage: `The user isn't exist`,
+        });
+      }
+      if (user) {
+        await db.User.destroy({
+          where: { id: userId },
+        });
+      }
+      resolve({
+        errCode: 0,
+        errMessage: `The user is deleted`,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let updateUserData = (data) =>{
+  return new Promise(async (resolve, reject) =>{
+    try{
+      if (!data.id) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required parameter",
+        });
+      }
+      let user = await db.User.findOne({
+        where : {id : data.id},
+        raw: false
+      });
+      if(user){
+        user.firstName = data.firstName;
+        user.lastName = data.lastName;
+        user.address = data.address;
+        await user.save();
+        resolve({
+          errCode: 0,
+          message: "Update the user succeed!"
+        });
+      }
+      else{
+        resolve({
+          errCode: 2,
+          errMessage: `User's not found!`,
+        });
+      }
+    }catch(e){
+      reject(e);
+    }
+  });
+};
+
 module.exports = {
   handleUserLogin: handleUserLogin,
   checkUserEmail: checkUserEmail,
-  getAllUsers: getAllUsers
+  getAllUsers: getAllUsers,
+  createNewUser: createNewUser,
+  hashUserPassword : hashUserPassword,
+  deleteUser: deleteUser,
+  updateUserData: updateUserData
 };
