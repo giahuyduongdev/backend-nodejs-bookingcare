931f1ec061649a0e6182071f54256b0b1987d9b0
giahuyduongdev <giahuyduongdev@gmail.com>
2024-11-11 22:25:31 +0700

    Config cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,24 @@ import cors from "cors";
 require('dotenv').config();
 
 let app = express();
-app.use(cors({ credentials: true, origin: true }));
+//app.use(cors({ credentials: true, origin: true }));
+
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", process.env.URL_REACT);
+
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+  
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+  
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
 
 
 app.use(bodyParser.json());
